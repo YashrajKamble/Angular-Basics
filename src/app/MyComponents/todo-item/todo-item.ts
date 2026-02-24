295bcb64d67d36b96dcf06bdc80c2e957be5f5ae
Yashraj_K <yashrajkamble7727@gmail.com>
2026-02-24 17:54:42 +0530

    Event Emitter in Angular

--- src/app/MyComponents/todo-item/todo-item.ts
+++ src/app/MyComponents/todo-item/todo-item.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Todo } from '../../Todo';
 
 @Component({
@@ -10,4 +10,10 @@ import { Todo } from '../../Todo';
 })
 export class TodoItem {
   @Input() todo!: Todo;
+  @Output() todoDelete: EventEmitter<Todo> = new EventEmitter();
+
+  onClick(todo: Todo) {
+    this.todoDelete.emit(todo);
+    console.log('onclick has been triggered');
+  }
 }
