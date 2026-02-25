30988e7e750cf4e8dc401110c751655246d76964
Yashraj_K <yashrajkamble7727@gmail.com>
2026-02-25 17:24:22 +0530

    Added Router Link and Router Outlet

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { Todos } from './MyComponents/todos/todos';
+import { RouterOutlet, RouterLinkWithHref } from "@angular/router";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [Todos],
+  imports: [Todos, RouterOutlet, RouterLinkWithHref],
   templateUrl: './app.html',
   styleUrls: ['./app.css'],
 })
